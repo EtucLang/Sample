88a3a6453bc4dbf0550f50dde304a652540ad9bf
EtucLang <justinhalltaa83@gmail.com>
2024-11-22 11:37:45 +0800

    Add project files.

---
+++ Sample/Sample.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    int age;
+
+    cout << "Hello student\n" << "Enter your name here: ";
+    cin >> name;
+
+    cout << "How old are you? ";
+    cin >> age;
+
+
+    return 0;
+
+}
